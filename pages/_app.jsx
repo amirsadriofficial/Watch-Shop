cbd8353c6d60fa07e7da338e9c1dd429f746a613
Amir Sadri <amirsadriofficial@gmail.com>
2021-10-11 03:24:50 +0330

    Update

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
 
+// eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />
 }
 export default MyApp
